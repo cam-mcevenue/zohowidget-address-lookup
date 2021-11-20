59eb6024d9933b824735e6fcdf248ef9ceb9e40b
Cameron McEvenue <cameronmcevenue@Camerons-MacBook-Pro.local>
2021-11-20 17:10:24 -0500

    Updated main.js to only intialize Google Maps components once the API is loaded

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import App from './App.svelte';
 
 const app = new App({
 	target: document.body,
+	props: {
+		mapsReady: false,
+	}
 });
+
+window.initMap = function ready() {
+	app.$set({ mapsReady : true });
+}
 
 export default app;
